7187ca82953a61f2a7b93416f2edd2a414b2905b
ALUtech166 <70975693+ALUtech166@users.noreply.github.com>
2020-11-28 15:00:40 +0200

    Update book.py

--- book.py
+++ book.py
@@ -1,9 +1,15 @@
 # Calling the parent class
 from item import Item
+""" This method is calling the parent class to the child class
+"""
 
 # Calling a second parent class
 from audiobook import Audiobook
+"""This is importing the audiobook file to the book file
+"""
 
 # Creating a child class
 class Book(Item, Audiobook):
+    """This is a child class for our Book(Item, Audiobook) with its characteristics
+    """
     pass
