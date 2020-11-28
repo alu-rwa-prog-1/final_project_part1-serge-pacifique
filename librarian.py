7cdf577a03503ebe1527b070f1463ec455d52f92
ALUtech166 <70975693+ALUtech166@users.noreply.github.com>
2020-11-28 15:19:59 +0200

    Update librarian.py

--- librarian.py
+++ librarian.py
@@ -1,21 +1,33 @@
 # Class Creation
 class Librarian:
+    """ This is a Liberian Class and the client can be able to add, sell, rent, borrow, and Revoke_Access
+    """ 
     # Add method
     def add(self):
+        """ This method allow the Liberian to add Item 
+        """
         pass
 
     # Sell method
     def sell(self):
+        """ This method allow the Liberian to sell Item
+        """
         pass
 
     # Renting method
     def rent(self):
+        """ This method allow the Liberian to rent Item
+        """
         pass
 
     # Borrow method
     def borrow(self):
+        """ This method allow the Liberian to Borrow Item
+        """
         pass
 
     # Revoke_Access method
     def revoke_access(self):
+        """ This method the Liberian to revoke_access Item
+        """
         pass
