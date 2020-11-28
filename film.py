d8cb637750d4a115f9966549a425530b4afcc349
ALUtech166 <70975693+ALUtech166@users.noreply.github.com>
2020-11-28 15:08:54 +0200

    Update film.py

--- film.py
+++ film.py
@@ -1,11 +1,17 @@
 # Calling the parent class
 from item import Item
+""" this importing item file to the film file
+"""
 
 # Calling a second parent class
 from audiobook import Audiobook
+""" this importing the audiobook file to the film file
+"""
 
 # Creating a child class
 class Film(Item, Audiobook):
+    """ This a Film class with its attributes
+    """
     def __init__(self, title, genre,no_item, donor_name, type, released_year):
      super().__init__(title, genre,no_item, donor_name)
      self.type = type
