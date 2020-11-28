475954ca0ae6208f70dc31c619c14affcbeec030
ALUtech166 <70975693+ALUtech166@users.noreply.github.com>
2020-11-28 15:14:23 +0200

    Update item.py

--- item.py
+++ item.py
@@ -1,5 +1,7 @@
 # Creating a class
 class Item:
+    """ This is a Item class with its characteristics
+    """
     def __init__(self, title, genre, no_item, donor_name, selling_price):
         self.title = title
         self.genre = genre
