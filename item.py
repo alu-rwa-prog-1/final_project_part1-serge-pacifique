b171727113c89cf9daa5c499be4d21783b744500
Linda Pacifique <71096252+IkireziPacifique@users.noreply.github.com>
2020-11-29 09:46:34 +0200

    third commit

--- item.py
+++ item.py
@@ -2,15 +2,10 @@
 class Item:
     """ This is a Item class with its characteristics
     """
-    def __init__(self, title, genre, no_item, donor_name, selling_price):
+    def __init__(self, title="", genre="", no_item=0, donor_name="", selling_price=0):
         self.title = title
         self.genre = genre
         self.no_item = no_item
         self.donor_name = donor_name
-
-    # def title(self):
-    #     pass
-    #
-    # def genre(self):
-    #     pass
+        self.selling_price = selling_price
 
