c4845d91302eb2dc89e0ba3ae93f656756540424
Linda Pacifique <71096252+IkireziPacifique@users.noreply.github.com>
2020-11-29 09:43:53 +0200

    third commit

--- audiobook.py
+++ audiobook.py
@@ -5,9 +5,9 @@ from item import Item
 class Audiobook(Item):
     """This is a class child for our Audiobook with its specifications
     """
-    def __init__(self, title, genre, no_item, donor_name, author, published_year, rental_price, selling_price):
+    def __init__(self, title = "", genre = "", no_item =0, donor_name="", author="",
+                 published_year = 0, rental_price = 0):
         super().__init__(title, genre, no_item, donor_name)
         self.author = author
         self.published_year = published_year
         self.rental_price = rental_price
-        self.selling_price = selling_price
