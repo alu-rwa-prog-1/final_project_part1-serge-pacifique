e8c17104cc4834086dd2a4a0db9ddf330b13481b
ALUtech166 <70975693+ALUtech166@users.noreply.github.com>
2020-11-30 14:34:02 +0200

    Update test_audiobook.py

--- test_audiobook.py
+++ test_audiobook.py
@@ -1,1 +1,20 @@
 import unittest
+
+from audiobook import Audiobook(Item)
+
+class Test_audiobook(unittest.TestCase):
+  """ This is our test class for audiobook
+  """
+  
+  def test_init(test):
+    The 100 = audiobook("The 100")
+    self.assertEqual(The 100.title, " The 100")
+    Chinua Achebe = audiobook("Chinua Achebe")
+    self.assertNotEqual(Chinua Achiebe.author, "Obama")
+    2020 = audiobook("2020")
+    self.assertEqual(2020.published_year, "2020")
+    
+     
+    
+  if __name__ == "__main__"
+  unittest.main()
