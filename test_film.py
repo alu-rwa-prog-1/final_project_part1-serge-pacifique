17e9954a4b38730a3099896a3d61853e8283d160
ALUtech166 <70975693+ALUtech166@users.noreply.github.com>
2020-11-30 14:52:18 +0200

    Update test_film.py

--- test_film.py
+++ test_film.py
@@ -1,1 +1,17 @@
 import unittest
+
+from film import Film
+
+class Test_Film(unittest.TestCase):
+  """ The Test case for Film
+  """
+  
+  def Test_init(test):
+    Action film = Film("Action film")
+    self.assertEqual(Action film.type, "Action film")
+    2019 = Film("2019")
+    self.assertNotEqual(2019.released_year, "2020")
+    
+    
+  if__name__ = "__main__":
+    unittest.main()
