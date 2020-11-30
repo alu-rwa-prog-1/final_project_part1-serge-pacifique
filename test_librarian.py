439909cd20a2b51a0614a937719dee1ee5081c99
ALUtech166 <70975693+ALUtech166@users.noreply.github.com>
2020-11-30 15:24:42 +0200

    Update test_librarian.py

--- test_librarian.py
+++ test_librarian.py
@@ -7,5 +7,31 @@ class Test_librarian(unittest.TestCase):
   """ This a Librarian Test
   """
   
-  def test_init(test):
+  def add(self):
+    """ This a testing code of the add method
+    """
+    book = Librarian("book")
+    self.assertEqual(book.type_item, "book")
+    self.assertNotEqual(book.type_item, "audiobook")
+    
+    
+  def sell(self):
+    """ Testing code of sell method
+    """
+    
+    
+  def rent(self):
+    """ Testing code of rent method
+    """
+    
+  def view(self):
+    """ Testing code of view method
+    """
+    
+  def lend(self):
+    """ Testing code of lend method
+    """
+      
+      
+    
     
